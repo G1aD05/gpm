cefec69b0b0fadf0bd32eefe8aec632e9382b7eb
G1aD05 <ezrag2000@proton.me>
2024-11-26 22:12:46 -0800

    I WAS IN THE WRONG DIRECTORY WHILE COMMITTING

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gpm
+gpm is a package manager that is planned to be better than the awful [OrbitPM](https://github.com/G1aD05/OrbitPM)\
+I hope to continue working on this\
+Also this doesn't work yet I've just made all the files and directories so far.
--- gpm/machinery/install.py
+++ gpm/machinery/install.py
@@ -1,5 +1,12 @@
 import urllib.request as request
+import pip
+import tarfile
+import os
+from typing import *
 
 
-def install(package: str):
-    pass
+def install(package: str, build_packages: Optional[List]):
+    os.chdir(f"{os.path.dirname(os.path.abspath(__file__))}/../../build")
+    request.urlretrieve(f"https://raw.githubusercontent.com/G1ad05/gpm/refs/heads/master/packages/{package}.tar.gz/", f"{package}.tar.gz")
+    with tarfile.open(f"{package}.tar.gz", "r:gz") as tar:
+        tar.extractall()
