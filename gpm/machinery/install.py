1a74fdb1cb8be9791b463ff725eded839c3f16c4
G1aD05 <ezrag2000@proton.me>
2024-11-27 11:41:17 -0800

    Created 'archives' directory

--- gpm/machinery/install.py
+++ gpm/machinery/install.py
@@ -5,8 +5,9 @@ import os
 from typing import *
 
 
-def install(package: str, build_packages: Optional[List]):
+def install(package: str, build_packages: list = Optional[List]):
     os.chdir(f"{os.path.dirname(os.path.abspath(__file__))}/../../build")
     request.urlretrieve(f"https://raw.githubusercontent.com/G1ad05/gpm/refs/heads/master/packages/{package}.tar.gz/", f"{package}.tar.gz")
     with tarfile.open(f"{package}.tar.gz", "r:gz") as tar:
         tar.extractall()
+
